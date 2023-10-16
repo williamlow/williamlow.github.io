c77a5f0f5b8952835463f2755dd7cfc1697a7889
williamlow <44437051+williamlow@users.noreply.github.com>
2023-10-16 13:22:55 +0100

    Date fix

--- ABM/report-NGA-biweekly-20231002/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
+++ ABM/report-NGA-biweekly-20231002/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -1,5 +1,5 @@
 
 var reportFreq = "biweekly"
 var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)"
-var conflict_period = "02 Oct 23 - 14 Oct 23"
+var conflict_period = "02 Oct 23 - 15 Oct 23"
 var report_period = "02 Oct 23 - 31 Dec 23"
