81b4bac106e9879a3620e0ca8b9252d9e758a0d4
williamlow <44437051+williamlow@users.noreply.github.com>
2023-11-13 14:42:07 +0000

    Adding report 20231030

---
+++ ABM/report-NGA-biweekly-20231030/860b193510eb3e219fc5938cecfe2331275827cd/data/data_conflict.js
@@ -0,0 +1,467 @@
+var confData ={
+  "type": "FeatureCollection",
+  "name": "conflict_data",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-09",
+        "location": "Dongo, Taraba",
+        "fatalities": 0,
+        "detail": "Four OAG/ISWAP operatives on two motorcycles arrived at the area and abducted a male civilian from his residence. The operatives suspect the individual of being a GF/NA informant."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.4507, 11.5752]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-08",
+        "location": "Pemi, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. When they opened the door, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.9166, 10.7768]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-08",
+        "location": "Wagga Lawan, Borno",
+        "fatalities": 0,
+        "detail": "Five criminals with handguns walked to the location and abducted two male farmers while the rest fled. Later the same day, the criminals contacted the relatives of the abductees and demanded a two-million-naira ransom for each."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6453, 10.9223]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-08",
+        "location": "Kilangar, Borno",
+        "fatalities": 2,
+        "detail": "OAG/ISWAP operatives on motorcycles arrived at a farmland where they attacked and killed two male farmers with bladed weapons."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.7626, 10.7058]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-08",
+        "location": "Mafa, Borno",
+        "fatalities": 3,
+        "detail": "GF/NA and GF/CJTF members, acting on a tip-off, pretended to be farmers and attacked OAG/JAS operatives while receiving a ransom of two million naira for the six farmers they abducted IVO the same location on 06.11.23. GF shot and killed three operatives and recovered the six abductees and a rifle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6002, 11.9239]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-07",
+        "location": "Mandaragirau, Taraba",
+        "fatalities": 1,
+        "detail": "Unidentified individuals with firearms broke into a private residence and attempted to abduct a male occupant. The occupant attempted to flee, but the individuals shot and killed him."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.3293, 10.6942]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-06",
+        "location": "Kelle, Borno",
+        "fatalities": 0,
+        "detail": "Five criminals armed with handguns arrived at a herdsmen settlement and fired shots in the air. They abducted one herdsman and rustled cattle before they left. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2054, 10.6938]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-06",
+        "location": "Mafa, Borno",
+        "fatalities": 0,
+        "detail": "OAG/JAS operatives with rifles arrived at a farmland on two vehicles and six motorcycles and abducted six farmers. The operatives contacted the family members of the abductees on 07.11.23 and demanded two million naira as ransom for the six abductees. GF/NA and GF/CJTF members, acting on a tip-off, rescued the six abductees IVO the same location on 08.11.23."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6002, 11.9239]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-06",
+        "location": "Marte, Borno",
+        "fatalities": 30,
+        "detail": "OAG/ISWAP operatives with rifles arrived at a logging site and attacked and killed at least 30 male loggers."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.8302, 12.365]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-06",
+        "location": "Koshebe, Borno",
+        "fatalities": 1,
+        "detail": "OAG/JAS operatives with rifles arrived at farmlands and attacked farmers. They stabbed and killed one and injured two others. GF/CJTF on patrol recovered the corpse around 1000hrs."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3179, 11.9764]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Hong, Adamawa",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupant to open the door or risk being shot. The occupants complied and opened the door. The criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.9304, 10.2317]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Mildo, Mildo",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupant to open the door or risk being shot. The occupants complied, prompting the criminals to abduct a male occupant before leaving."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.579, 10.8013]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Mubi, Adamawa",
+        "fatalities": 0,
+        "detail": "Five criminals armed with knives stole a mobile phone from a male civilian while walking along a secondary road. They attacked and injured him for resisting."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2722, 10.266]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Bargozo, Taraba",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles abducted five male civilians for hunting pigs in the area. Reportedly, the operatives have warned civilians in the area to stop hunting pigs, which they considered forbidden."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.6035, 11.6602]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Karkur, Borno",
+        "fatalities": 10,
+        "detail": "OAG/JAS operatives with rifles arrived at farmlands. They killed one male farmer by slitting his throat and stabbed and injured another. The operatives told the victim to inform GF/NA that they would soon attack the Khaddamari and Zabarmari villages of Jere LGA. The operatives then arrived at another farmland within the same area around 1845hrs, where they killed nine male farmers by slitting their throats. They used one of these victims' phones to contact his family. They instructed the family to bring 500 thousand naira for the return of the corpses."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2697, 12.0316]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Kalwa, Borno",
+        "fatalities": 5,
+        "detail": "OAG operatives with rifles arrived at a logging site and rounded up ten loggers. They shot and killed five and injured two others while some escaped."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7061, 12.5025]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Kellumiri, Borno",
+        "fatalities": 1,
+        "detail": "Seven OAG/ISWAP operatives with rifles walked to farmland and fired shots in the air, prompting the farmers to flee. They attempted to abduct an off-duty GF/CJTF, but subsequently shot and killed him after he refused to go with the operatives."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6679, 11.5734]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Ngaranna, Borno",
+        "fatalities": 15,
+        "detail": "Several OAG/ISWAP operatives with rifles arrived at a logging site on several motorcycles and fired rifles sporadically, prompting the loggers to flee. They shot and killed 15 male loggers, but two others escaped with gunshot injuries."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7276, 12.6058]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-05",
+        "location": "Monguno, Borno",
+        "fatalities": 0,
+        "detail": "Four unidentified individuals armed with pump action rifles broke into an LNGO facility and stole two solar batteries and a handbag belonging to a staff member. Reportedly, they arrived and threatened to kill the security guard if he refused to open the door. They entered the facility and beat him up after he complied. They further threatened to kill him the next time they came."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6167, 12.6713]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-04",
+        "location": "Karkur, Borno",
+        "fatalities": 0,
+        "detail": "OAG/JAS operatives with rifles arrived at farmlands and abducted five male farmers. Subsequently, the operatives called the family members of the abductees and demanded a one-million-naira ransom for each abductee. OAG/ISWAP operatives rescued the abductees on 05.11.23."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2659, 12.0308]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Gombe, Gombe",
+        "fatalities": 0,
+        "detail": "Three Kalare gang members armed with knives robbed a male and female civilian of their mobile phones while walking by the roadside. They also injured the male when he resisted."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.1653, 10.2841]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Mafa, Borno",
+        "fatalities": 0,
+        "detail": "OAG/ISWAP operatives with rifles walked to a logging site and fired shots sporadically, prompting the loggers to flee. They abducted four male loggers before they left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6002, 11.9239]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Konduga, Borno",
+        "fatalities": 0,
+        "detail": "A 40-year-old man raped a 10-year-old girl at his private residence. GF/CJTF members, acting on a tip-off, apprehended the perpetrator and handed him over to GF/NPF."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.4179, 11.6533]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Kukawa, Borno",
+        "fatalities": 2,
+        "detail": "OAG/ISWAP operatives with rifles arrived at a logging site on three motorcycles and fired shots sporadically, prompting the loggers to flee. They rounded up eight male loggers while fleeing. They killed two and flogged the other six before allowing them to leave. Reportedly, the operatives had restricted the loggers of the logging around the location."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Ngurno Kanumbri, Ngurno Kanumbri",
+        "fatalities": 2,
+        "detail": "OAG operatives with rifles shot and killed two male civilians and injured one other. The motivation for the attack is unknown."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7002, 12.7146]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-03",
+        "location": "Njimtilo, Borno",
+        "fatalities": 1,
+        "detail": "Unidentified individuals broke into a fuel station and killed the local security guard by slitting his throat. The reason behind the attack is unknown."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.0219, 11.8495]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-02",
+        "location": "Lawanti, Borno",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles entered farmlands and kidnapped five male farmers. They also stole two tricycles and a vehicle belonging to the farmers. The operatives called the abductees' family members and demanded an undisclosed amount of ransom."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.4806, 11.5851]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-02",
+        "location": "Kartari, Adamawa",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles aboard a light truck and on several motorcycles stopped a commercial vehicle in transit while they were crossing the road. They forced the driver to disembark from the vehicle. They saw him chewing tobacco and cut off his lips. They also searched for other passengers’ identification before they allowed them to continue their journey."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.8017, 12.4219]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-02",
+        "location": "Yola, Adamawa",
+        "fatalities": 0,
+        "detail": "Five male criminals armed with handguns broke into the private residence of the Chief of Staff to the Speaker of the Adamawa State House of Assembly and kidnapped him. They called the victim's family and demanded a ransom of 20 million naira."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.4493, 9.2565]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-01",
+        "location": "Michika, Adamawa",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3888, 10.6205]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-01",
+        "location": "Askira, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted two male occupants and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.9132, 10.6517]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-10-31",
+        "location": "Kukawa, Borno",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles on two motorcycles arrived at the location and abducted two male civilians from their private residences."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-10-31",
+        "location": "Zarmari, Borno",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles rounded up civilians logging for firewood in the area, abducted three loggers, and also siphoned fuel from the loggers’ tricycles. Reportedly, the operatives were armed with new rifles."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5073, 11.6136]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-10-31",
+        "location": "Gumsa, Borno",
+        "fatalities": 10,
+        "detail": "Civilians aboard a pickup tricycle hit an OAG road-planted IED while in transit along a secondary road. The explosion killed 10 civilians and injured at least three others. Reportedly, the civilians were coming from the burial ceremony of the victims killed by OAG operatives in Isanari village, Gumsa ward, Geidam LGA, Yobe State, on 30.10.2023."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.801, 12.5609]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-10-30",
+        "location": "Gumsa, Borno",
+        "fatalities": 16,
+        "detail": "OAG operatives with rifles entered the location and fired shots sporadically at the residents. They shot and killed at least 16 civilians and injured at least two others, while others fled. The operatives proceeded and set several houses on fire before withdrawing. On 31.10.23 at about 0900hrs, some civilians who came for the burial ceremony of the victims hit an OAG road-planted IED IVO Mittiri, Gumsa, Geidam LGA, Yobe State."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.801, 12.5609]
+      }
+    }
+  ]
+}
---
+++ ABM/report-NGA-biweekly-20231030/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -0,0 +1,5 @@
+
+  var reportFreq = "biweekly";
+  var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)";
+  var conflict_period = "30 Oct 23 - 12 Nov 23";
+  var report_period = "30 Oct 23 - 28 Jan 24"
