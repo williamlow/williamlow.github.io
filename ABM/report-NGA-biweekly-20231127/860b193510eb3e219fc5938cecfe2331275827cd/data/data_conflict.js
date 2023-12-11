2deb57905921624ea3629bde76ed4af3ba707219
williamlow <44437051+williamlow@users.noreply.github.com>
2023-12-11 16:21:48 +0000

    report 2023 11 27

---
+++ ABM/report-NGA-biweekly-20231127/860b193510eb3e219fc5938cecfe2331275827cd/data/data_conflict.js
@@ -0,0 +1,519 @@
+var confData ={
+  "type": "FeatureCollection",
+  "name": "conflict_data",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-28",
+        "location": "Nasarawo, Nasarawo",
+        "fatalities": 0,
+        "detail": "Two male criminals armed with sticks attacked and injured a lady standing by the roadside. They stole her phone before they left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0475, 9.4499]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-30",
+        "location": "Jimeta, Adamawa",
+        "fatalities": 0,
+        "detail": "Shilla gang members armed with knives confronted a woman aboard a commercial tricycle and snatched her handbag and mobile phone while in transit along a secondary road. They stabbed and injured her with a knife for resisting."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.4573, 9.2788]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-02",
+        "location": "Pakka, Pakka",
+        "fatalities": 0,
+        "detail": " Criminals armed with handguns infiltrated a herdsmen settlement and fired shots in the air, prompting the herdsmen to flee. They abducted two herdsmen and rustled their cattle before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2178, 10.1123]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-03",
+        "location": "Vimtim , Adamawa",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3124, 10.3301]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-06",
+        "location": "Yola, Adamawa",
+        "fatalities": 0,
+        "detail": "Civilians discovered a newborn female baby abandoned by an unknown individual and reported the incident to GF/NPF who later recovered her alive. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.4833, 9.2]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Giwano, Giwano",
+        "fatalities": 0,
+        "detail": "Two unidentified individuals armed with sticks attacked and injured a private motorcycle rider while in transit along a secondary road. Subsequently, they stole the motorcycle and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.8833, 9.5667]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-04",
+        "location": "Girei, Adamawa",
+        "fatalities": 0,
+        "detail": "Two Shilla gang members assaulted and injured a lady while walking along a secondary road. They also stole her mobile phone and handbag before leaving. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.5451, 9.3628]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Wagga Lawan, Wagga Lawan",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns threatened the occupants to open the door of a private residence or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6453, 10.9223]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-28",
+        "location": "Takaskala, Takaskala",
+        "fatalities": 0,
+        "detail": "OAG/JAS operatives with rifles attacked a GF/NA position, prompting GF to return fire. They exchanged fire briefly until the operatives retreated. During the armed clash, the operatives shot and injured a GF member. One farmer sustained injuries from a stray bullet."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6711, 10.9847]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-27",
+        "location": "Ngulde, Ngulde",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.5446, 10.6661]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-28",
+        "location": "Sama'a, Sama'a",
+        "fatalities": 0,
+        "detail": "OAG/JAS operatives arrived at a riverine area and opened small arms fire at fishermen. During the attack, they abducted two fishermen while others escaped."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3465, 11.6448]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-30",
+        "location": "Dunge, Dunge",
+        "fatalities": 3,
+        "detail": "OAG/ISWAP operatives with rifles arrived at a riverine area on motorcycles and abducted eight fishermen. They shot and killed three of them for resisting."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2686, 12.0282]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-30",
+        "location": "Pulka, Borno",
+        "fatalities": 1,
+        "detail": "At least 10 OAG/JAS operatives with rifles walked to a farmland and fired shots in the air, prompting the farmers to flee. The operatives shot and killed one male farmer and stole six bicycles before leaving."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7813, 11.225]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-29",
+        "location": "Garubula, Garubula",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns arrived at a farmland and abducted a male farmer on a farmland. On 04.12.2023 the criminals released the abductee after receiving 1,000,000 naira ransom."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0189, 10.7775]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-27",
+        "location": "Dikwa, Borno",
+        "fatalities": 0,
+        "detail": "OAG/ISWAP operatives with rifles infiltrated a herdsmen settlement, abducted five female civilians, and rustled 150 cows, donkeys, sheep, and rams before withdrawing. On 30.11.2023 the operatives released the abductees for unknown reasons."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.9187, 12.0349]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-29",
+        "location": "Maiduguri, Borno",
+        "fatalities": 0,
+        "detail": "Three criminals armed with knives, disguised as a tricycle rider and passengers, snatched a mobile phone and bag from a female passenger. They injured her and pushed her out of the tricycle afterward."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1536, 11.8395]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-30",
+        "location": "Muna Dalti, Muna Dalti",
+        "fatalities": 0,
+        "detail": "Two criminals armed with sticks disguised as passengers attacked and injured a tricycle rider. They then stole the tricycle and left. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2445, 11.8684]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-11-30",
+        "location": "Ngulde, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupant to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.5446, 10.6661]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-01",
+        "location": "Dikwa, Borno",
+        "fatalities": 0,
+        "detail": "GF/NA acting on a tip-off, arrested an OAG operative and a female family member. They also recovered a bicycle and other valuables."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.9187, 12.0349]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-02",
+        "location": "Monguno, Borno",
+        "fatalities": 2,
+        "detail": "OAG/ISWAP operatives with rifles aboard light trucks and motorcycles arrived at the northern outskirts of the location and parked their vehicles. They proceeded on foot and attacked a GF/NA position, prompting GF to return fire. The armed clash lasted for several minutes, and the operatives retreated. During the armed clash, the operatives shot and killed two GF members. The stray bullet injured a child during the incident."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6167, 12.6713]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-02",
+        "location": "Ngulde, Ngulde",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.5446, 10.6661]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-03",
+        "location": "Galtimari, Galtimari",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles arrived at farmland and shot sporadically in the air, prompting farmers to flee. They abducted three male farmers and left. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3963, 11.6858]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-04",
+        "location": "Firgi, Firgi",
+        "fatalities": 2,
+        "detail": "A commercial tricycle hit an OAG road-planted IED while in transit along the road. The explosion injured a male and a female civilian and killed two young girls."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7837, 11.2666]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Naira Gana, Naira Gana",
+        "fatalities": 1,
+        "detail": "A male civilian stepped on an OAG road-planted IED along a secondary road and the explosion killed him."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.419, 13.6992]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Maiduguri, Borno",
+        "fatalities": 0,
+        "detail": "BOGIS (Borno Geographic Information System) staff arrived at the location and instructed the traders to leave. Two traders confronted them, and as a result, BOGIS staff injured and then arrested the traders. Reportedly, the Borno state government gave a one-week ultimatum to the traders in the Post Office Area and Tashan Bama to vacate the locations."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1536, 11.8395]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Grade, Grade",
+        "fatalities": 1,
+        "detail": "OAG/ISWAP with rifles arrived at the location and killed a male civilian after they found him in possession of several tramadol tablets, allegedly for recreational use."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.8558, 12.7508]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-03",
+        "location": "Titiwa, Titiwa",
+        "fatalities": 1,
+        "detail": "Five unidentified criminals armed with handguns set up an informal VCP along a secondary road. They fired shots at a commercial vehicle that refused to stop at the VCP and left the area. The shots killed the driver and partly damaged the vehicle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.8995, 12.1415]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-04",
+        "location": "Kauji Kura, Kauji Kura",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns and machetes entered farmlands where they abducted one civilian and left. Subsequently, the abductee was released on 05.12.2023 at about 1200hrs upon payment of a 250,000-naira ransom."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.7041, 11.0398]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-06",
+        "location": "Dalori, Borno",
+        "fatalities": 0,
+        "detail": "OAG/JAS operatives with rifles arrived at a herdsmen settlement and attempted to rustle cattle. Herdsmen armed with arrows confronted the operatives, they clashed for several minutes before the operatives retreated upon the arrival of GF/NA. During the encounter, the herdsmen destroyed one motorcycle belonging to the operatives, while the operatives shot and injured one herdsman."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2661, 11.7601]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-06",
+        "location": "Mallam Bayari, Mallam Bayari",
+        "fatalities": 1,
+        "detail": "A male logger stepped on an OAG road-planted IED along a secondary road. The resulting blast killed him and injured another male logger."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7561, 12.7684]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Mallam Bayari, Mallam Bayari",
+        "fatalities": 0,
+        "detail": "OAG/ISWAP operatives with rifles aboard five light trucks arrived at the location and abducted six male loggers."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7561, 12.7684]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-06",
+        "location": "Kilangar, Kilangar",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns threatened the occupants to open the door of a private residence or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.7626, 10.7058]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Pemi, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns threatened the occupants of a private residence to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.9166, 10.7768]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Pulka, Borno",
+        "fatalities": 1,
+        "detail": "During the Borno state deputy governor livestock distribution exercise, GF/NA armed with sticks beat a woman and a child carried on her back. The woman was injured in the incident and the child was killed. The woman later reported the incident to the National Human Rights Commission."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7813, 11.225]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Gumnari, Gumnari",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles on motorcycles set up an informal VCP along a secondary road. They stopped two commercial trucks in transit and abducted at least two female and three male passengers from the two vehicles, as well as the two drivers. They also stole the vehicles and shot another one with rifles for refusing to stop, injuring the driver and damaging the vehicle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7256, 12.6243]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Dandalmari, Dandalmari",
+        "fatalities": 0,
+        "detail": "Unidentified criminals armed with rifles arrived at the herdsmen settlement where they abducted one civilian and left. On the same day, they contacted the family of the abductee and demanded a ten million-naira ransom. This incident is linked to nga:6572e6a8"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.6629, 11.7962]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-07",
+        "location": "Dandalmari, Dandalmari",
+        "fatalities": 0,
+        "detail": "GF/NA arrested two civilians, carrying seven million naira, on their way to pay the ransom for an abducted family member which occurred two days prior. GF/NA interrogated the civilians about the abduction and then arrested them. This incident is connected to nga:6572e29f."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.6629, 11.7962]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Gombe, Gombe",
+        "fatalities": 0,
+        "detail": "Three Kalare gang members armed with machetes and knives attacked and injured two male civilians while walking along a secondary road for unknown reasons."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.1653, 10.2841]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2023-12-05",
+        "location": "Buni Yadi, Yobe",
+        "fatalities": 0,
+        "detail": "A female individual armed with a stick attacked and injured her husband following a domestic dispute. The victim was taken to the hospital and GF/NPF members arrested the woman."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0077, 11.2761]
+      }
+    }
+  ]
+}
---
+++ ABM/report-NGA-biweekly-20231127/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -0,0 +1,5 @@
+
+  var reportFreq = "biweekly";
+  var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)";
+  var conflict_period = "27 Nov 23 - 10 Dec 23";
+  var report_period = "27 Nov 23 - 25 Feb 24"
