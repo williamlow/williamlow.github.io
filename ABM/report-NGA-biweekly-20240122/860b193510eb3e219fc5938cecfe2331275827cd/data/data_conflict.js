a9991a51a15e5b4a8960ee2ad6201d8befda068b
williamlow <william.d.low@gmail.com>
2024-02-06 23:14:13 +0000

    Report 20240122

---
+++ ABM/report-NGA-biweekly-20240122/860b193510eb3e219fc5938cecfe2331275827cd/data/data_conflict.js
@@ -0,0 +1,389 @@
+var confData ={
+  "type": "FeatureCollection",
+  "name": "conflict_data",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Lafia, Nasawara",
+        "fatalities": 0,
+        "detail": "A 30-year-old man lured a 15-year-old into an uncompleted building and raped her. Shortly after, GF/NPF, acting on a tip-off, arrested him."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.8177, 9.6519]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-22",
+        "location": "Gipalma, Gipalma",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupant to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted one male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3059, 10.2748]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-28",
+        "location": "Nasarawo, Nasawara",
+        "fatalities": 0,
+        "detail": "A male criminal armed with a knife stabbed and injured a male civilian following a dispute. Nearby civilians apprehended the criminal, handed him over to GF/NPF and rushed the injured to a hospital."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0475, 9.4499]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-30",
+        "location": "Yinagu, Yinagu",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot. The occupants complied and opened the door. Subsequently, the criminals abducted a male occupant and left. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.344, 10.9185]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-30",
+        "location": "Gipalma, Gipalma",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and threatened the occupants to open the door or risk being shot.The occupants complied and opened the the door. Subsequently, the criminals abducted a male occupant and left."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3059, 10.2748]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-22",
+        "location": "Chingowa, Chingowa",
+        "fatalities": 0,
+        "detail": "Twelve OAG operatives with rifles on motorcycles set up an informal VCP along a secondary road. They stopped a commercial truck ferrying foodstuffs carjacked the vehicle and abducted the driver."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.6918, 12.255]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-22",
+        "location": "Bakassi, Cross River",
+        "fatalities": 5,
+        "detail": "A commercial vehicle hit an OAG road-planted IED while in transit along the road. The resulting blast killed five passengers and injured two others."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.8826, 12.0059]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-22",
+        "location": "Gajibo, Gajibo",
+        "fatalities": 7,
+        "detail": "A commercial vehicle hit an OAG road planted IED while in transit along the road. The resulting blast killed seven male civilians and damaged the vehicle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.9895, 12.1101]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-23",
+        "location": "Mogula, Mogula",
+        "fatalities": 0,
+        "detail": "A commercial vehicle hit an OAG road-planted IED while in transit along the road. The resulting blast damaged the vehicle and injured at least two passengers aboard."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [14.0057, 12.1545]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Dille, Yobe",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns infiltrated a herdsmen settlement and fired shots into the air, prompting the herdsmen to flee. They subsequently, rustled the cattle and abducted a herdsman before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2155, 10.7513]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Auno, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns broke into a private residence and abducted a male civilian. While leaving, the male civilian overpowered one of the criminals and escaped. The criminals went back to the residence and abducted his child and wife. The criminals later called and demanded an undisclosed amount of money as ransom."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.9375, 11.8482]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-25",
+        "location": "Sama'a, Borno",
+        "fatalities": 32,
+        "detail": "OAG operatives killed 32 civilians all by slitting their throats. The operatives accused them of being GF informants following an attack on the operatives by a group of hunters, which led to the killing of three operatives in the area. Reportedly, the loggers have an informal agreement with the operatives to allow them to conduct livelihood activities in the area. The incident is linked to nga:65b8afbb and nga:65b91458."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3465, 11.6448]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-25",
+        "location": "Doron Baga, Borno",
+        "fatalities": 6,
+        "detail": "OAG/ISWAP operatives on boats opened small arms fire and killed six fishermen. They threatened the rest to vacate the area until they further announced for them to return. The operatives then stole at least 30 boats before leaving."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.8608, 13.1165]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-25",
+        "location": "Mussa, Mussa",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns infiltrated a herdsmen settlement and fired shots into the air, prompting the herdsmen to flee. They subsequently, rustled cattle and abducted two herdsmen before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1307, 10.7357]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-26",
+        "location": "Mogula, Mogula",
+        "fatalities": 0,
+        "detail": "A commercial vehicle hit an OAG road planted IED along the road. The resulting blast injured one passenger and damaged the vehicle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [14.0057, 12.1545]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-27",
+        "location": "Gubio, Borno",
+        "fatalities": 1,
+        "detail": "A suspected UXO/ERW device exploded at a metal scrap-selling shop. The explosion killed six children and a male civilian. The UXO was part of the metals collected by the scavengers on the outskirts of the town."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.7848, 12.499]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-28",
+        "location": "Mijigine, Borno",
+        "fatalities": 1,
+        "detail": "OAG/ISWAP with rifles on motorcycles arrived at a herdsmen settlement and rustled at least 30 cattle. They shot and killed a herdsman for an unknown reason."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7745, 12.1063]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-29",
+        "location": "Kukawa, Borno",
+        "fatalities": 0,
+        "detail": "OAG/ISWAP operatives with rifles on motorcycles arrived at the location and abducted a male civilian from his private residence for an unknown reason."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-29",
+        "location": "Firgi, Firgi",
+        "fatalities": 9,
+        "detail": "A commercial vehicle hit an OAG/ISWAP road-planted IED while in transit along a secondary road. The explosion damaged the vehicle, killed nine males, and five females, and injured two females and five male passengers. ISWAP claimed the attack via Amaq on 01.02.2024."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.7837, 11.2666]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-28",
+        "location": "Kukawa, Borno",
+        "fatalities": 2,
+        "detail": "OAG/ISWAP operatives with rifles arrived at the location and abducted two male civilians. One of the abductees is reported to be an elderly Imam while the other is a young man who sells pharmaceutical drugs by the roadside. ISWAP claimed to have captured and killed two spies of the Nigerian army via Amaq on 29.01.2024. The incident is linked to nga:65b9ff51."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Sama'a, Borno",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles abducted at least 32 male civilians, logging for firewood and charcoal in the area. They accused them of being GF informants. The incident is linked to nga:65b8afbb and nga:65b28012"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3465, 11.6448]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-30",
+        "location": "Huyim Town, Huyim Town",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns infiltrated a herdsmen settlement and fired shots into the air, prompting the herdsmen to flee. They subsequently, rustled cattle and abducted two herdsmen before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1446, 10.7681]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-30",
+        "location": "Leho, Leho",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns infiltrated a herdsmen settlement and fired shots into the air, prompting the herdsmen to flee. They subsequently, rustled cattle and abducted one herdsman before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.8443, 10.7034]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-31",
+        "location": "Kukawa, Boron",
+        "fatalities": 3,
+        "detail": "OAG/ISWAP operatives with rifles on motorcycles arrived at the location and abducted three male civilians. At about 1600hrs the same day the operatives shot and killed the abductees for an unknown reason."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Tumfure, Gombe",
+        "fatalities": 0,
+        "detail": "At least eight criminals, two armed with AK-47 rifles and others with machetes, broke into four private residences in the area and robbed the occupants of money and other valuables. They also injured a male and a female civilian who resisted. The criminals fired several gunshots in the air before leaving."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.0834, 10.2849]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-24",
+        "location": "Gombe, Gombe",
+        "fatalities": 0,
+        "detail": " Two Kalare gang members armed with knives aboard a motorcycle stopped a male civilian walking along a secondary road and attempted to rob him. The civilian managed to escape with minor wounds."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.1653, 10.2841]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-26",
+        "location": "Lalaipido, Gombe",
+        "fatalities": 1,
+        "detail": "Criminals armed with knives stabbed and killed a male civilian and robbed him of his valuables."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.2507, 9.7917]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-26",
+        "location": "Gashua, Yobe",
+        "fatalities": 0,
+        "detail": "A male criminal armed with a knife stabbed and injured a male civilian following a dispute over a woman. Nearby civilians apprehended the criminal, handed him over to GF/NPF and rushed the injured to a hospital."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.0388, 12.8729]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-01-31",
+        "location": "Chirawa, Chirawa",
+        "fatalities": 0,
+        "detail": "Two criminals armed with iron rods pretended to be passengers and stopped a commercial motorcycle rider while in transit along a secondary road. They hit him and robbed him of his motorcycle."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.318, 12.8453]
+      }
+    }
+  ]
+}
---
+++ ABM/report-NGA-biweekly-20240122/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -0,0 +1,5 @@
+
+  var reportFreq = "biweekly";
+  var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)";
+  var conflict_period = "22 Jan 24 - 04 Feb 24";
+  var report_period = "22 Jan 24 - 21 Apr 24"
