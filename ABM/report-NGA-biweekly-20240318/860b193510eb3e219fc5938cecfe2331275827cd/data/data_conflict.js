4a9962b282c6d5ba4f7fc2031860c994b1d606f5
williamlow <william.d.low@gmail.com>
2024-04-04 15:58:38 +0100

    Report 2024 03 18

---
+++ ABM/report-NGA-biweekly-20240318/860b193510eb3e219fc5938cecfe2331275827cd/data/data_conflict.js
@@ -0,0 +1,415 @@
+var confData ={
+  "type": "FeatureCollection",
+  "name": "conflict_data",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-18",
+        "location": "Duhu, Adamawa",
+        "fatalities": 0,
+        "detail": "A male civilian armed with a knife stabbed and injured another male civilian following a dispute over a woman. GF/NPF, acting on a tip-off, arrested the perpetrator."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.4512, 10.7875]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-21",
+        "location": "Ngbalang, Adamawa",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns kidnapped a male civilian in front of his residence as he was returning home. They called the victim's family and demanded 2 million naira ransom."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0218, 9.5179]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-22",
+        "location": "Bakin Dutse, Adamawa",
+        "fatalities": 0,
+        "detail": "A 46-year-old male individual lured a nine-year-old girl to his private residence and raped her. The victim raised an alarm prompting nearby civilians to mobilise and apprehend the perpetrator. They handed him over to vigilante members and took the victim to a hospital."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.475, 10.8351]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-23",
+        "location": "Garkida, Adamawa",
+        "fatalities": 1,
+        "detail": "OAG/ISWAP operatives with rifles arrived at the location and attacked a GF/NA position stationed at the outskirts, sparking a heavy armed clash that forced civilians to flee from their homes for safety. The exchange of fire lasted for two hours until the operatives retreated. During the clash, a stray bullet hit and killed a female civilian as she was fleeing. ISWAP claimed the attack via Amaq on 25.03.2024."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.5663, 10.4145]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-27",
+        "location": "Kircinga, Adamawa",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles on motorcycles arrived at the outskirts of the location and parked their motorcycles at the security trench. They infiltrated the location on foot, entered a private residence, and abducted a male civilian. They also robbed him of an unspecified amount of money. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.3556, 10.7828]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-28",
+        "location": "Shuwa, Borno",
+        "fatalities": 0,
+        "detail": "Seven OAG operatives with rifles on foot infiltrated the location, entered a private residence of a suspected drug peddler, and robbed him of six million naira. The operatives abducted the victim, injured him, and later released him at the outskirts of the location. The victim returned to his home in the morning hours of the same day."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.415, 10.7476]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-29",
+        "location": "Zuyel, Adamawa",
+        "fatalities": 0,
+        "detail": "Ten OAG/JAS operatives with rifles infiltrated a herdsmen settlement and fired shots in the air. They rustled a herd of cattle and abducted a herdsman to help them move with the cattle easily. The operatives released the abductee on the outskirts of the area and he returned home safely."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.4852, 10.774]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-18",
+        "location": "Maiduguri, Borno",
+        "fatalities": 2,
+        "detail": "A fire erupted in an IDP camp and killed seven children, two male civilians, and burned 100 tents, foodstuffs, and other valuables. The fire also damaged several INGO properties such as Health facilities, Site management shed/office, Tom brown kitchens, CCCM shelters, Makeshift shelters, Emergency shelters, Boreholes, Water points, Solar streetlights, Blocks of Latrines, Suggestion boxes and Waste collection bins."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1536, 11.8395]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-18",
+        "location": "Kukawa, Borno",
+        "fatalities": 2,
+        "detail": "OAG/ISWAP operatives with rifles on four motorcycles arrived at the location, entered private residences and abducted four male civilians. Subsequently, they shot and killed two abductees before leaving."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.5662, 12.9248]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-20",
+        "location": "Mussa, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with handguns infiltrated a herdsmen settlement and fired shots into the air, prompting the herdsmen to flee. They subsequently, rustled cattle and abducted two herdsmen before withdrawing. Shortly after, the criminal contacted the family members of the abductees and demanded a ransom of 2,000,000 naira."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.1307, 10.7357]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-20",
+        "location": "Mafa, Borno",
+        "fatalities": 0,
+        "detail": "OAG operatives with rifles on seven motorcycles arrived at a herdsmen settlement, abducted two herdsmen, and rustled at least 200 cows. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6002, 11.9239]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-21",
+        "location": "Kwatar Mashaya, Borno",
+        "fatalities": 5,
+        "detail": "OAG operatives with rifles arrived at the location and shot and killed five farmers for an unknown reason. Shortly after, operatives’ commanders from Sabon Tumbum, Marte LGA arrived at the area and apprehended two of the operatives who perpetrated the act."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.8748, 12.6967]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-20",
+        "location": "Ngetra Lawanti, Ngetra Lawanti",
+        "fatalities": 1,
+        "detail": "Criminals with rifles on foot set up an informal VCP along a secondary road. They stopped a commercial vehicle in transit and robbed the passengers of money and other valuables. They shot and killed a male civilian who attempted to flee. The criminals left afterwards."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.6807, 12.5838]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-21",
+        "location": "Mafa, Borno",
+        "fatalities": 3,
+        "detail": "A fire erupted in an IDP camp, killed three children, and burned 160 tents, foodstuffs, and other valuables. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6002, 11.9239]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-23",
+        "location": "Mangada, Borno",
+        "fatalities": 0,
+        "detail": "Criminals armed with sticks and machetes infiltrated a herdsmen settlement and attempted to abduct a herdsman. They attacked and injured him with sticks for refusing to go with them, prompting him to raise an alarm. The criminals fled upon sighting civilians approaching."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.1275, 10.8117]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-25",
+        "location": "Monguno, Borno",
+        "fatalities": 0,
+        "detail": "IDPs protested against an NGO during a food distribution exercise following some changes in their distribution process. Reportedly, the NGO initially distributed without vendors but invited a vendor during this exercise. The beneficiaries revolted about the process accusing the vendors of improper distribution of the item. This resulted in the protesters assaulting and injuring two volunteers, pelting stones and partially damaging two NGO vehicles, shelters and other valuables. Two protesters were injured as a result of the stampede. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6167, 12.6713]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-26",
+        "location": "Jakana, Borno",
+        "fatalities": 1,
+        "detail": " OAG/ISWAP operatives with rifles attacked a vigilante position on the outskirts of the town, prompting the vigilante to return fire briefly until the operatives retreated. No casualties were reported. Another group of operatives silently infiltrated the location, broke into the private residence of an Imam, and killed him by slitting his throat. ISWAP claimed the attack via An-Naba' on 28.03.2024."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.7747, 11.845]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-26",
+        "location": "Gwoza, Borno",
+        "fatalities": 0,
+        "detail": "GF/NA member stabbed and injured a male civilian for unknown reasons. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6942, 11.0831]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-26",
+        "location": "Gwoza, Borno",
+        "fatalities": 0,
+        "detail": "A GF/NA member while walking along a secondary road stabbed and injured a tricycle rider after he nearly hit him because of reckless riding. Nearby residents attempted to protest but other GF members calmed the situation."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6942, 11.0831]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-27",
+        "location": "Dille, Adamawa",
+        "fatalities": 3,
+        "detail": "OAG operatives with rifles on motorcycles arrived at farmlands and opened small arms fire at farmers. They shot and killed three farmers and two farmers sustained injuries while fleeing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.2155, 10.7513]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-26",
+        "location": "Kwaple, Kwaple",
+        "fatalities": 1,
+        "detail": "OAG operatives with rifles on motorcycles arrived at the location and began shooting sporadically, prompting the civilians to flee. The operatives shot and killed a male civilian and rustled cattle before withdrawing."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.8035, 10.9087]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-28",
+        "location": "Goniri, Yobe",
+        "fatalities": 0,
+        "detail": "OAG/ISWAP operatives with rifles fired shots on the outskirts of the location and retreated upon sighting GF/NA approaching. Three civilians were hit and injured by stray bullets."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.6738, 11.5058]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-25",
+        "location": "Kulani, Borno",
+        "fatalities": 1,
+        "detail": "Criminals armed with knives stabbed and killed a male civilian and dumped his corpse by the roadside. The circumstances surrounding the killing are unknown."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.6663, 9.7747]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-25",
+        "location": "Kurecin Giye, Kurecin Giye",
+        "fatalities": 0,
+        "detail": "OAG/Bandits abducted thirteen residents comprising of 8 females, 3 children and 2 males. They also rustled animals, and stole food during an attack. Residents said the motorcycle-riding bandits came with AK-47 rifles and shot indiscriminately. "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [7.3539, 12.3183]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-20",
+        "location": "Buni Yadi, Yobe",
+        "fatalities": 0,
+        "detail": "Criminals with handguns infiltrated a herdsmen settlement, broke into the private residence of a community leader, and shot and injured him when they did not find money in the residence. Vigilantes on patrol deployed to the location and arrested one of the criminals while the others fled."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.0077, 11.2761]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-19",
+        "location": "Dapchi, Yobe",
+        "fatalities": 1,
+        "detail": " A male criminal armed with a knife stabbed and killed a male civilian during an argument. The criminal was apprehended and handed over to GF/NPF."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.4944, 12.4904]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-21",
+        "location": "Gashua, Yobe",
+        "fatalities": 0,
+        "detail": "Two criminals armed with knives on a motorcycle stopped a male civilian walking by the roadside with a bag of rice and robbed him of the rice. They also stabbed and injured him when he resisted."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.0388, 12.8729]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-23",
+        "location": "Gashua, Yobe",
+        "fatalities": 1,
+        "detail": "A fire erupted from a cooking stove in the university hostel. The fire killed a pregnant woman and burned buildings, foodstuff, money, and valuables. Shortly after the incident, the school authority suspended the academic activities until further notice."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.0388, 12.8729]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-26",
+        "location": "Magazu, Magazu",
+        "fatalities": 2,
+        "detail": "OAG/Bandits killed two male civilians and abducted two others during an attack on Magazu community in Tsafe LGA of Zamfara State. The OAG, carrying rifles, shot indiscriminately, forcing residents to run for safety."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.9517, 11.8978]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-24",
+        "location": "Gidan Zuma, Gidan Zuma",
+        "fatalities": 4,
+        "detail": "OAG/Bandits with rifles attacked Gidan Zuma village, shot four female residents to death, and abducted 11 others. They also abducted four men and 3 children."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.8869, 12.1939]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "date": "2024-03-27",
+        "location": "Wanke, Wanke",
+        "fatalities": 1,
+        "detail": "OAG/Bandits fired at a moving vehicle, killed one passenger, and abducted five others. Other passengers ran into Wanke community for safety. The attack occurred on the Gusau-Dansadau road."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [6.6368, 12.0549]
+      }
+    }
+  ]
+}
---
+++ ABM/report-NGA-biweekly-20240318/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -0,0 +1,5 @@
+
+  var reportFreq = "biweekly";
+  var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)";
+  var conflict_period = "18 Mar 24 - 31 Mar 24";
+  var report_period = "18 Mar 24 - 16 Jun 24"
