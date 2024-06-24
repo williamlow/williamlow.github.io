c99ac2ef8632fc8727540240f6ab8d0f3a3cbdda
williamlow <william.d.low@gmail.com>
2024-06-24 22:15:59 +0100

    ABM report June 02

---
+++ ABM/report-NGA-biweekly-20240602/860b193510eb3e219fc5938cecfe2331275827cd/data/data_report.js
@@ -0,0 +1,5 @@
+
+  var reportFreq = "biweekly";
+  var geography = "Northeast Nigeria (Adamawa, Borno and Yobe states)";
+  var conflict_period = "10 Jun 24 - 23 Jun 24";
+  var report_period = "10 Jun 24 - 08 Sep 24"
